2f15d5776f47c4103610c63f9ab6202ac44f1d96
serega4517 <serega4517@yandex.ru>
2022-07-03 18:22:42 +0300

    feat: pages Login and Register done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,36 @@
+import "../Register/Register.css"
+import "./Login.css";
+import logo from "../../images/logo.svg";
+import {Link} from "react-router-dom";
+
 const Login = () => {
   return (
-    <h1>Страница авторизации</h1>
+    <section className="reg">
+      <img className="reg__logo" src={logo} />
+      <h1 className="reg__title">Рады видеть!</h1>
+      <form className="reg__form">
+        <label className="reg__label">
+          <h3 className="label__text">E-mail</h3>
+          <input className="reg__input"
+                 type="email"
+                 autoComplete="off"
+                 required
+          />
+        </label>
+        <label className="reg__label">
+          <h3 className="label__text">Пароль</h3>
+          <input className="reg__input"
+                 type="password"
+                 autoComplete="off"
+                 required
+          />
+        </label>
+        <button className="reg__button_type_login" type="submit">Войти</button>
+      </form>
+      <p className="reg__text">Еще не зарегистрированы?&nbsp;
+        <Link to="/signup" className="reg__text reg__link">Регистрация</Link>
+      </p>
+    </section>
   )
 }
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,6 +1,44 @@
+import "./Register.css";
+import logo from "../../images/logo.svg";
+
+import { Link } from "react-router-dom";
+
 const Register = () => {
   return (
-    <h1>Страница регистрации</h1>
+    <section className="reg">
+      <img className="reg__logo" src={logo} />
+      <h1 className="reg__title">Добро пожаловать!</h1>
+      <form className="reg__form">
+        <label className="reg__label">
+          <h3 className="label__text">Имя</h3>
+          <input className="reg__input"
+                 type="text"
+                 autoComplete="off"
+                 required
+          />
+        </label>
+        <label className="reg__label">
+          <h3 className="label__text">E-mail</h3>
+          <input className="reg__input"
+                 type="email"
+                 autoComplete="off"
+                 required
+          />
+        </label>
+        <label className="reg__label">
+          <h3 className="label__text">Пароль</h3>
+          <input className="reg__input"
+                 type="password"
+                 autoComplete="off"
+                 required
+          />
+        </label>
+        <button className="reg__button" type="submit">Зарегистрироваться</button>
+      </form>
+      <p className="reg__text">Уже зарегистрированы?&nbsp;
+        <Link to="/signin" className="reg__text reg__link">Войти</Link>
+      </p>
+    </section>
   )
 }
 
