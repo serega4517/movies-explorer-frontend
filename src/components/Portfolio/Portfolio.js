9c6cd5ac3da1cf8613cbdda72a3afc3a49839bf5
serega4517 <serega4517@yandex.ru>
2022-07-09 21:44:31 +0300

    feat: component Portfolio done

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import './Portfolio.css';
+
+const Portfolio = () => {
+  return (
+    <section className="portfolio">
+      <div className="portfolio__container">
+        <h2 className="portfolio__heading">Портфолио</h2>
+        <ul className="portfolio__projects">
+          <li className="portfolio__project-item">
+            <p className="portfolio__project">Статичный сайт</p>
+            <a className="portfolio__project-link" href="#" target="_blank"></a>
+          </li>
+          <li className="portfolio__project-item">
+            <p className="portfolio__project">Адаптивный сайт</p>
+            <a className="portfolio__project-link" href="#" target="_blank"></a>
+          </li>
+          <li className="portfolio__project-item">
+            <p className="portfolio__project">Одностраничное приложение</p>
+            <a className="portfolio__project-link" href="#" target="_blank"></a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default Portfolio;
