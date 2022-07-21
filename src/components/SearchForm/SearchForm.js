7a6ca87ec98d314390946db51fe88af9bc2ac895
serega4517 <serega4517@yandex.ru>
2022-07-21 18:45:07 +0300

    fix: enclosed the search button in the tag <button> and moved the required attribute to the desired input

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,16 +4,24 @@ import './SearchForm.css';
 
 const SearchForm = () => {
   return (
-    <form className="search-form" name="search-form">
+    <form className="search-form"
+          name="search-form"
+    >
       <div className="search-form__input-container">
-        <input className="search-form__input" type="text" placeholder="Фильм"/>
-        <a className="search-form__button" href="#" />
+        <input className="search-form__input"
+               type="text"
+               placeholder="Фильм"
+               required
+        />
+        <button className="search-form__button"
+                type="submit"
+                onClick={(e => e.preventDefault())}
+        />
       </div>
       <div className="search-form__checkbox-container">
         <input className="search-form__checkbox"
                type="checkbox"
                id="switch"
-               required
         />
         <label className="search-form__checkbox_type_toggle" htmlFor="switch" />
         <p className="search-form__checkbox-name">Короткометражки</p>
